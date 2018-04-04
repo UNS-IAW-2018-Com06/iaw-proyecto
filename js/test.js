b6691b2d8a415be5ad6ef5869cf796c999605ebd
Luciano <lucho.fuentez@gmail.com>
2018-04-04 20:14:01 -0300

    Test JQuery

--- js/test.js
+++ js/test.js
@@ -4,9 +4,22 @@ var universidades;
 $(function() {
     $.get("./data/universidades.json", function(data, status) {
         console.log(data);
-        //pedido = recuperarPedido();
         //universidades = new Map(data.map((universidad) => [getId(universidad), universidad]));
-        //mostrarPizzas(ordenarPizzas(data));
-        //actualizarTotal(computarTotal());
+        mostrarUniversidades(data);
     });
 });
+
+function mostrarUniversidades(data){
+    var index
+
+    $.each(data, function(index,universidad){
+        var id = getId(universidad);
+        agregarUniversidad(id,universidad);
+    })
+} 
+
+function agregarUniversidad(id,universidad){
+    var row = $("<tr></tr>").attr("id", id);
+    row.append($("<td></td").text(universidad.nombre));
+    $("#tabla").append(row);
+}
