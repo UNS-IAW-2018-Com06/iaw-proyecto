17e6071a095c6025f65b3a780fbe2badc365be26
DESKTOP-5F1ACM6\monica <leandro18195@hotmail.com>
2018-04-06 13:47:34 -0300

    cambiar estilos V1

---
+++ js/estilo.js
@@ -0,0 +1,19 @@
+
+
+function cambiarEstilo1(){
+   $("body").css("background-color", "yellowgreen");
+   $(".container-fluid").css({"text-align": "left", "background-color":"yellowgreen" });
+
+}
+function cambiarEstilo2(){
+    $("body").css("background-color", "whitesmoke");
+    $(".container-fluid").css("background-color","whitesmoke");
+ }
+
+ /*
+$("#btn_estilos").click(function(e){
+  e.preventDefault();
+  var rutaEstilo = $(this).attr("href")
+  $("#linkestilo").attr("href", rutaEstilo)
+})
+*/
--- js/test.js
+++ js/test.js
@@ -1,11 +1,9 @@
 
 var universidades;
-//var estilo;
 
 $(function() {
     $.get("./data/universidades.json", function(data, status) {
         console.log(data);
-       // estilo = recuperarEstilo();
         universidades = new Map(data.map((universidad) => [getId(universidad), universidad]));
         mostrarUniversidades(data);
     });
@@ -31,22 +29,3 @@ function agregarUniversidad(id,universidad){
     $("#tabla").append(row);
 }
 
-/*function onActualizarEstilo(e) {
-    var id = $(e.target).parents("tr").attr("id");
-
-    //le cambio el estado
-    actualizarEstado(id, actualizarEstilo(id));
-
-    guardarEstilo(estilo);
-}
-*/
-/*function actualizarEstilo(id) {
-    var enPedido = !pedido.has(id);
-    if (enPedido) {
-        pedido.add(id);
-    } else {
-        pedido.delete(id);
-    }
-    return enPedido;
-}
-*/
