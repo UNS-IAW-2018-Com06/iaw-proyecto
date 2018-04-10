b2cf76d736dd05dfaf276b069757d125820ebde4
Luciano Fuentes <lucho.fuentez@gmail.com>
2018-04-10 16:00:53 -0300

    Cambios

--- js/comentariosLocalStorage.js
+++ js/comentariosLocalStorage.js
@@ -1,13 +1,18 @@
-function recuperarComentarios() {
-    var comentariosDeUniversidad = localStorage.comentario;
-    if(comentariosDeUniversidad == undefined){
-        return "";
-    }
-    else{
-        return comentariosDeUniversidad; 
-    }
-}
-
-function guardarComentario(comentario) {
-   localStorage.comentario = JSON.stringify(comentario);
-}
+function guardarComentario(id,comentario) {
+    var lista = JSON.parse(localStorage.getItem(id));
+    lista.push(comentario);
+    localStorage.setItem(id,JSON.stringify(lista));
+ }
+ 
+ 
+ function recuperarComentarios(id) {
+     var lista = JSON.parse(localStorage.getItem(id));
+     if(lista){
+         return lista;
+     }
+     else{
+         var lista = [];
+         localStorage.setItem(id,JSON.stringify(lista));
+         return lista;
+     }
+ }
