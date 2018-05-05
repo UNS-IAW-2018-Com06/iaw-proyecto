1d69c1c8065f7dc06352ec75686b9af1c12284b3
Luciano Fuentes <lucho.fuentez@gmail.com>
2018-05-05 17:22:32 -0300

    Update facebook

--- app_server/auth/facebook.js
+++ app_server/auth/facebook.js
@@ -6,7 +6,7 @@ var Users = require('../models/users');
 passport.use(new FacebookStrategy({
     clientID: '580439325646370',
     clientSecret: 'ad98705353412d3e21d8b646cb15bb0d',
-    callbackURL: "http://localhost:3000/"
+    callbackURL: "https://unimapoteca.herokuapp.com/"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
